67cfb3623d457f9449c90e4266c51973482dd8f0
esc0rtd3w <esc0rtd3w@gmail.com>
2018-05-03 00:19:53 -0400

    Added recovery directory

---
+++ recovery/tv/read_recovery.cmd
@@ -0,0 +1,4 @@
+@echo off
+
+
+adb shell dd if=/dev/block/platform/msm_sdcc.1/by-name/recovery of=/sdcard/recovery.img
---
+++ recovery/tv/write_cwm_tv.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+pause
+
+adb shell dd if=/sdcard/recovery.img of=/dev/block/platform/msm_sdcc.1/by-name/recovery
