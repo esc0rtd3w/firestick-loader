f98535ec4c89fb80381f5770388d06e6e1893a65
esc0rtd3w <esc0rtd3w@gmail.com>
2018-05-12 21:32:21 -0400

    Added Script To Pull Package Usage List *root*

---
+++ scripts/misc/pull-package-usage-list.cmd
@@ -0,0 +1,55 @@
+@echo off
+
+color 0e
+
+set _color="%cd%\bin\cocolor.exe"
+
+set extractRAR="%~dp0bin\rar.exe" -y x
+set extractZIP="%~dp0bin\unzip.exe" -o
+
+set sleep="%~dp0bin\wait.exe"
+set rm=rmdir /s /q
+
+set adb="..\..\bin\adb.exe"
+
+set appName=0
+set choice=2
+
+set install=%adb% install
+set uninstall=%adb% uninstall
+set push=%adb% push
+set pull=%adb% pull
+set shell=%adb% shell
+
+set dumpFolder="%temp%\firestick-loader\sdcard-dump"
+
+set swipeUp=%shell% input swipe 200 900 200 300
+set keyEnter=%shell% input keyevent 66
+set keyTab=%shell% input keyevent 61
+
+
+set sdcard=sdcard
+::set sdcard=external_sd
+::set sdcard=extSdCard
+
+if exist %dumpFolder% rd /q /s %dumpFolder%
+mkdir %dumpFolder%
+
+echo.
+echo.
+echo.
+echo Dumping From /%sdcard%/...
+echo.
+echo.
+
+
+%shell% "su -c cp -R /data/system/package-usage.list /%sdcard%/package-usage.list"
+
+%pull% %sdcard%/package-usage.list %dumpFolder%
+
+start "" explorer %dumpFolder%
+
+:end
+
+
+
