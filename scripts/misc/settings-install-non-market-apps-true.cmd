9e2585be3915a7e94d4b804e966aedfd9efb2001
esc0rtd3w <esc0rtd3w@gmail.com>
2018-05-25 19:13:41 -0400

    Added New Settings Scripts

---
+++ scripts/misc/settings-install-non-market-apps-true.cmd
@@ -0,0 +1,15 @@
+@echo off
+
+color 0b
+
+set adb="..\..\bin\adb.exe"
+set shell=%adb% shell
+
+
+cls
+echo Settings^: secure install_non_market_apps 1
+
+%shell% settings --user 0 put secure install_non_market_apps 1
+
+exit
+
