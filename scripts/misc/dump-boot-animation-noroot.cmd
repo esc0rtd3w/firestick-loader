b5f5423d41d787b0bc94a82be73319d02cd0f2db
esc0rtd3w <esc0rtd3w@gmail.com>
2019-03-15 03:22:15 -0400

    Added "No Root" Script Variations

---
+++ scripts/misc/dump-boot-animation-noroot.cmd
@@ -0,0 +1,55 @@
+@echo off
+
+color 0e
+
+set _color="%cd%\bin\cocolor.exe"
+
+set extractRAR="%~dp0bin\rar.exe" -y x
+set extractZIP="%~dp0bin\unzip.exe" -o
+
+set sleep="%~dp0bin\wait.exe"
+set rm=rmdir /s /q
+
+set adb="..\..\bin\adb.exe"
+
+set appName=0
+set choice=2
+
+set install=%adb% install
+set uninstall=%adb% uninstall
+set push=%adb% push
+set pull=%adb% pull
+set shell=%adb% shell
+
+set dumpFolder="%temp%\firestick-loader\sdcard-dump"
+
+set swipeUp=%shell% input swipe 200 900 200 300
+set keyEnter=%shell% input keyevent 66
+set keyTab=%shell% input keyevent 61
+
+
+set sdcard=sdcard
+::set sdcard=external_sd
+::set sdcard=extSdCard
+
+if exist %dumpFolder% rd /q /s %dumpFolder%
+mkdir %dumpFolder%
+
+echo.
+echo.
+echo.
+echo Dumping From /%sdcard%/...
+echo.
+echo.
+
+
+%shell% "cp /system/media/bootanimation.zip /%sdcard%/bootanimation.zip"
+
+%pull% %sdcard%/bootanimation.zip %dumpFolder%
+
+start "" explorer %dumpFolder%
+
+:end
+
+
+
