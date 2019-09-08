7a20f0a0ab4c0a6be53181f5875eede9b4367177
Jason Borowski <esc0rtd3w@gmail.com>
2019-09-08 04:14:22 -0400

    Fixed Script

--- scripts/misc/install-apk.cmd
+++ scripts/misc/install-apk.cmd
@@ -6,13 +6,19 @@ color 0e
 
 
 set adb="..\..\bin\adb.exe"
-
-
 set install=%adb% install
 
 
 
 cls
+set apk=0
+
+cls
+echo Type Name of Package or Drop and Press ENTER:
+echo.
+echo.
+
+set /p apk=
 
 
 %install% %apk%
