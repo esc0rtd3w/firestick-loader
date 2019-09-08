7dae10103993fd953297658bdadfae9d4e92c9f7
Jason Borowski <esc0rtd3w@gmail.com>
2019-09-08 03:50:19 -0400

    Added Standalone Mount Scripts

---
+++ scripts/misc/mount-system-as-rw.cmd
@@ -0,0 +1,28 @@
+@echo off
+
+color 0e
+
+
+set adb="..\..\bin\adb.exe"
+set adbKill="%~dp0bin\adb.exe" kill-server
+set adbStart="%~dp0bin\adb.exe" start-server
+set adbWait=%adb% wait-for-device
+
+set install=%adb% install
+set uninstall=%adb% uninstall
+set push=%adb% push
+set pull=%adb% pull
+set shell=%adb% shell
+
+
+%shell% "su -c mount -o remount,rw /system"
+
+echo.
+echo Success if not output or error!
+echo.
+pause
+
+:end
+
+
+
