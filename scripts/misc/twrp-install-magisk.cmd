b6c0dfc9c45cbe2ecd1daa4d98f7cb46a55374ef
Jason Borowski <esc0rtd3w@gmail.com>
2019-09-09 04:01:55 -0400

    Added Standalone Magisk Install Script

---
+++ scripts/misc/twrp-install-magisk.cmd
@@ -0,0 +1,38 @@
+@echo off
+
+color 0e
+
+
+set adb="..\..\bin\adb.exe"
+set adbKill="%~dp0bin\adb.exe" kill-server
+set adbStart="%~dp0bin\adb.exe" start-server
+set adbWait=%adb% wait-for-device
+
+set install=%adb% install
+set uninstall=%adb% uninstall
+set push=%adb% push
+set pull=%adb% pull
+set shell=%adb% shell
+
+
+set src=0
+
+cls
+echo Enter Source File Path or Drop File and Press ENTER:
+echo.
+echo.
+
+set /p src=
+for /r %%a in (type %src%) do (
+set name=%%~nxa
+)
+
+%push% %src% /sdcard/
+%shell% "twrp install /sdcard/%name%"
+
+
+
+:end
+
+
+
