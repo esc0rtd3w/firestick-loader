6ed8c19cd656649ba886c0cff2640429954e296b
Jason Borowski <esc0rtd3w@gmail.com>
2019-09-10 05:20:36 -0400

    Updated Post Debloat Data

--- data/tank/post-debloated/restore/com.adamioan.scriptrunner/cache/exec.sh
+++ data/tank/post-debloated/restore/com.adamioan.scriptrunner/cache/exec.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-su -c am start -a com.amazon.device.settings.action.MY_ACCOUNT -n com.amazon.tv.settings/com.amazon.tv.settings.tv.my_account.MyAccountActivity
+su -c am start -a com.amazon.device.settings.action.MANAGE_APPLICATIONS -n com.amazon.tv.settings/com.amazon.tv.settings.tv.applications.ApplicationsActivity
