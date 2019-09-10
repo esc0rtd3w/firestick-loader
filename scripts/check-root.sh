81b5f67d3a7786e86b7256d2136340d4e8aba295
Jason Borowski <esc0rtd3w@gmail.com>
2019-09-09 21:50:56 -0400

    Updated Root Detection
    Now checks
    
    - /system/bin/su
    - /system/xbin/su
    - /sbin/su

--- scripts/check-root.sh
+++ scripts/check-root.sh
@@ -11,12 +11,55 @@
 
 checkRootStatus(){
 	
-	if [ -f /system/xbin/su ];
+	chk_bin="0"
+	chk_sbin="0"
+	chk_xbin="0"
+	echo "0">/sdcard/rootAccess.txt
+	
+	if [ -f /system/bin/su ];
 	then
-		echo "1">/sdcard/rootAccess.txt
+		chk_bin="1"
 	else
-		echo "0">/sdcard/rootAccess.txt
+		chk_bin="0"
 	fi
+	
+	case "$chk_bin" in
+	
+		"0")
+			if [ -f /system/xbin/su ];
+			then
+				chk_xbin="1"
+			else
+				chk_xbin="0"
+			fi
+		;;
+	
+		"1")
+			echo "1">/sdcard/rootAccess.txt
+			break;
+		;;
+	
+	esac
+	
+	case "$chk_xbin" in
+	
+		"0")
+		if [ -f /sbin/su ];
+		then
+			chk_sbin="1"
+			echo "1">/sdcard/rootAccess.txt
+		else
+			chk_sbin="0"
+			echo "0">/sdcard/rootAccess.txt
+		fi
+		;;
+	
+		"1")
+			echo "1">/sdcard/rootAccess.txt
+			break;
+		;;
+	
+	esac
 	
 }
 	
