2e2ee31779c4e63c30285c2ea3ac37833d5c8f97
Jason Borowski <esc0rtd3w@gmail.com>
2019-09-10 03:53:48 -0400

    Added Scripts For Use With SH Script Runner Shortcuts

---
+++ scripts/settings/tank/system/scripts/my-account.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+su -c am start -a com.amazon.device.settings.action.MY_ACCOUNT -n com.amazon.tv.settings/com.amazon.tv.settings.tv.my_account.MyAccountActivity
---
+++ scripts/settings/tank/system/scripts/network.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+su -c am start -a com.amazon.device.settings.action.ADD_NETWORK -n com.amazon.tv.settings/com.amazon.tv.settings.tv.network.NetworkActivity
