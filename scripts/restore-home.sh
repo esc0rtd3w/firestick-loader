d99e0b9fb06f574258ea9ebf63a073cdd9a6b991
Jason Borowski <esc0rtd3w@gmail.com>
2019-09-13 05:52:11 -0400

    Updated Default Restore Home Script

--- scripts/restore-home.sh
+++ scripts/restore-home.sh
@@ -1,24 +1,24 @@
 #!/system/bin/sh
 
-
+mount -o remount,rw /system
 	
-if [ -d /data/ ];
-then
-	empty=0
-else
-	mkdir /data/
-	chmod -R 0771 /data/
-	chown -R system:system /data/
-fi
+#if [ -d /data/ ];
+#then
+#	empty=0
+#else
+#	mkdir /data/
+#	chmod -R 0771 /data/
+#	chown -R system:system /data/
+#fi
 	
-if [ -d /data/data/ ];
-then
-	empty=0
-else
-	mkdir /data/data/
-	chmod -R 0771 /data/data/
-	chown -R system:system /data/data/
-fi
+#if [ -d /data/data/ ];
+#then
+#	empty=0
+#else
+#	mkdir /data/data/
+#	chmod -R 0771 /data/data/
+#	chown -R system:system /data/data/
+#fi
 
 # Copy All Home User Data For Default Setup
 #cp -r /system/restore/ca.dstudio.atvlauncher.pro/ /data/data/
@@ -42,8 +42,20 @@ fi
 cp -r /system/restore/TitaniumBackup/ /sdcard/
 
 
+# Install Packages
+cp /system/restore/apk/autostarts.apk /data/local/tmp/
+pm install /data/local/tmp/autostarts.apk
+cp /system/restore/apk/busybox.apk /data/local/tmp/
+pm install /data/local/tmp/busybox.apk
+cp /system/restore/apk/mouse-toggle.apk /data/local/tmp/
+pm install /data/local/tmp/mouse-toggle.apk
+cp /system/restore/apk/reboot.apk /data/local/tmp/
+pm install /data/local/tmp/reboot.apk
+
+
 # Cleanup Extra Crap
 rm -r /system/res/images/*.*
 rm -r /system/res/sound/*.*
 
+mount -o remount,ro /system
 
