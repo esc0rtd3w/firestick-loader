d783517fa48dc944a9cfec183376e0f817b34f9f
Jason Borowski <esc0rtd3w@gmail.com>
2019-09-15 00:57:27 -0400

    Cleaned Up Comments

--- scripts/restore-home.sh
+++ scripts/restore-home.sh
@@ -1,35 +1,6 @@
 #!/system/bin/sh
 
 mount -o remount,rw /system
-	
-#if [ -d /data/ ];
-#then
-#	empty=0
-#else
-#	mkdir /data/
-#	chmod -R 0771 /data/
-#	chown -R system:system /data/
-#fi
-	
-#if [ -d /data/data/ ];
-#then
-#	empty=0
-#else
-#	mkdir /data/data/
-#	chmod -R 0771 /data/data/
-#	chown -R system:system /data/data/
-#fi
-
-# Copy All Home User Data For Default Setup
-#cp -r /system/restore/ca.dstudio.atvlauncher.pro/ /data/data/
-#cp -r /system/restore/com.adamioan.scriptrunner/ /data/data/
-#cp -r /system/restore/com.fluxii.android.mousetoggleforfiretv/ /data/data/
-#chmod -R 0755 /data/data/ca.dstudio.atvlauncher.pro/
-#chmod 0660 /data/data/ca.dstudio.atvlauncher.pro/databases/sections.db
-#chmod 0660 /data/data/ca.dstudio.atvlauncher.pro/databases/sections.db-shm
-#chmod 0660 /data/data/ca.dstudio.atvlauncher.pro/databases/sections.db-wal
-#chmod -R 0755 /data/data/com.adamioan.scriptrunner/
-#chmod -R 0755 /data/data/com.fluxii.android.mousetoggleforfiretv/
 
 # TitaniumBackup FTW ;)
 if [ -d /sdcard/TitaniumBackup/ ];
@@ -51,11 +22,6 @@ cp /system/restore/apk/mouse-toggle.apk /data/local/tmp/
 pm install /data/local/tmp/mouse-toggle.apk
 cp /system/restore/apk/reboot.apk /data/local/tmp/
 pm install /data/local/tmp/reboot.apk
-
-
-# Cleanup Extra Crap
-rm -r /system/res/images/*.*
-rm -r /system/res/sound/*.*
 
 mount -o remount,ro /system
 
