07a9c4b941bb8cc8cbc0847ebc20c7b58c8d31a2
Jason Borowski <esc0rtd3w@gmail.com>
2019-09-24 19:33:08 -0400

    Added Standalone Script To Remove Amazon OTA

---
+++ scripts/misc/remove-amazon-OTA.cmd
@@ -0,0 +1,46 @@
+@echo off
+
+:reset
+
+color 0e
+
+set _color="%cd%\bin\cocolor.exe"
+
+set extractRAR="%~dp0bin\rar.exe" -y x
+set extractZIP="%~dp0bin\unzip.exe" -o
+
+set sleep="%~dp0bin\wait.exe"
+set rm=rmdir /s /q
+
+set adb="..\..\bin\adb.exe"
+set adbWait=%adb% wait-for-device
+
+set appName=0
+set choice=2
+
+set install=%adb% install
+set uninstall=%adb% uninstall
+set push=%adb% push
+set pull=%adb% pull
+set shell=%adb% shell
+
+set swipeUp=%shell% input swipe 200 900 200 300
+set keyEnter=%shell% input keyevent 66
+set keyTab=%shell% input keyevent 61
+
+
+set sdcard=sdcard
+::set sdcard=external_sd
+::set sdcard=extSdCard
+
+
+
+cls
+echo Removing Amazon OTA....
+echo.
+echo.
+
+%shell% "su -c mount -o remount,rw /system"
+%sleep% 3
+%shell% "su -c rm -r /system/priv-app/DeviceSoftwareOTA/"
+%shell% "su -c rm -r /system/priv-app/DeviceSoftwareOTAIdleOverride/"
