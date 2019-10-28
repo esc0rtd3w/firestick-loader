d2a56ed673262310920e68a8b382840bb830530b
Jason Borowski <esc0rtd3w@gmail.com>
2019-10-28 04:01:41 -0400

    Updated Settings Scripts For Custom Home Data

---
+++ scripts/settings/tank/system/scripts/5272/preferences.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+su -c am start -a com.amazon.device.settings.action.PREFERENCES -n com.amazon.tv.settings.v2/.tv.preferences.PreferencesActivity
---
+++ scripts/settings/tank/system/scripts/alexa.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+su -c am start -a amazon.intent.action.ALEXA_SETTING -n com.amazon.vizzini/.setting.AlexaSettingActivity
