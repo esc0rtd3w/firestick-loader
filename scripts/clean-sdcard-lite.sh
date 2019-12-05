c5cd4c9f80e81d3ef3a82945206fa4f6482a152f
Jason Borowski <esc0rtd3w@gmail.com>
2019-12-05 06:00:54 -0500

    Updated Removal List

--- scripts/clean-sdcard-lite.sh
+++ scripts/clean-sdcard-lite.sh
@@ -6,8 +6,10 @@ rm -r /sdcard/Downloader
 rm -r /sdcard/Mobdro
 rm -r /sdcard/Movies
 rm -r /sdcard/Music
+rm -r /sdcard/show_box
 rm -r /sdcard/TWRP
 rm -r /sdcard/Typhoon
+rm -r /sdcard/time4popcorn
 rm -r /sdcard/TitaniumBackup
 
 mkdir /sdcard/CyberFlix
@@ -16,6 +18,8 @@ mkdir /sdcard/Downloader
 mkdir /sdcard/Mobdro
 mkdir /sdcard/Movies
 mkdir /sdcard/Music
+mkdir /sdcard/show_box
 mkdir /sdcard/TWRP
 mkdir /sdcard/Typhoon
+mkdir /sdcard/time4popcorn
 mkdir /sdcard/TitaniumBackup
