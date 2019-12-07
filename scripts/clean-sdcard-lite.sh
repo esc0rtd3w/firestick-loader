a9c4f7949bdc10857dae705debb51ac1699e4168
Jason Borowski <esc0rtd3w@gmail.com>
2019-12-07 03:18:55 -0500

    Updated Paths To Clean

--- scripts/clean-sdcard-lite.sh
+++ scripts/clean-sdcard-lite.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+rm  /sdcard/*.png
+rm  /sdcard/*.zip
+
 rm -r /sdcard/CyberFlix
+#rm -r /sdcard/data
 rm -r /sdcard/Download
 rm -r /sdcard/Downloader
 rm -r /sdcard/Mobdro
 rm -r /sdcard/Movies
 rm -r /sdcard/Music
+rm -r /sdcard/NovaTV
+rm -r /sdcard/Podcasts
+rm -r /sdcard/restore
 rm -r /sdcard/show_box
 rm -r /sdcard/TWRP
 rm -r /sdcard/Typhoon
@@ -13,11 +20,15 @@ rm -r /sdcard/time4popcorn
 rm -r /sdcard/TitaniumBackup
 
 mkdir /sdcard/CyberFlix
+#mkdir /sdcard/data
 mkdir /sdcard/Download
 mkdir /sdcard/Downloader
 mkdir /sdcard/Mobdro
 mkdir /sdcard/Movies
 mkdir /sdcard/Music
+mkdir /sdcard/NovaTV
+mkdir /sdcard/Podcasts
+mkdir /sdcard/restore
 mkdir /sdcard/show_box
 mkdir /sdcard/TWRP
 mkdir /sdcard/Typhoon
