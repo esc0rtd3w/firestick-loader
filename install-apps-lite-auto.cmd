59083351253e5b0cf1acc28988025e4c92c901ba
Jason Borowski <esc0rtd3w@gmail.com>
2019-12-14 23:25:16 -0500

    Added Twitch To Auto Apps

--- install-apps-lite-auto.cmd
+++ install-apps-lite-auto.cmd
@@ -287,6 +287,16 @@ echo.
 
 cls
 %_color% 0e
+set appName=Twitch
+set apk=
+echo.
+echo.
+echo Installing %appName%...
+echo.
+%install% "apps\media\twitch.apk"
+
+cls
+%_color% 0e
 set appName=VLC TV
 set apk=
 echo.
