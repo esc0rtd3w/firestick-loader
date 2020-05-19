f9c09225f8da1d022569ac7a1c1ce96e7418764c
Jason Borowski <esc0rtd3w@gmail.com>
2020-05-18 21:59:25 -0400

    Added AndroidKeyMapper To Map Keyboard Keys From PC To Android

---
+++ scripts/misc/android-key-mapper.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+title AndroidKeyMapper Launcher                      esc0rtd3w 2020
+
+
+
+set AndroidKeyMapper="..\..\bin\AndroidKeyMapper.exe"
+
+cls
+echo Use Your PC Keyboard Keys To Control Android Device
+echo.
+
+%AndroidKeyMapper%
+
+:end
+
+
