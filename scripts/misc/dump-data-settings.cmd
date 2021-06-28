3e8d02f4f50ad95c0c7c1695349615dfd4de6a85
Jason Borowski <esc0rtd3w@gmail.com>
2021-06-28 01:21:45 -0400

    Updated Remove List

--- scripts/misc/dump-data-settings.cmd
+++ scripts/misc/dump-data-settings.cmd
@@ -50,6 +50,8 @@ echo.
 %shell% "su -c rm -r %sdcard%/com.amazon.tv.launcher"
 %shell% "su -c rm -r %sdcard%/com.amazon.tv.oobe"
 %shell% "su -c rm -r %sdcard%/com.amazon.tv.settings"
+%shell% "su -c rm -r %sdcard%/com.amazon.tv.settings.core"
+%shell% "su -c rm -r %sdcard%/com.amazon.tv.settings.v2"
 %shell% "su -c rm -r %sdcard%/com.amazon.venezia"
 
 %shell% "su -c cp -R /data/data/com.amazon.device.settings /%sdcard%/com.amazon.device.settings"
@@ -58,6 +60,8 @@ echo.
 %shell% "su -c cp -R /data/data/com.amazon.tv.launcher /%sdcard%/com.amazon.tv.launcher"
 %shell% "su -c cp -R /data/data/com.amazon.tv.oobe /%sdcard%/com.amazon.tv.oobe"
 %shell% "su -c cp -R /data/data/com.amazon.tv.settings /%sdcard%/com.amazon.tv.settings"
+%shell% "su -c cp -R /data/data/com.amazon.tv.settings /%sdcard%/com.amazon.tv.settings.core"
+%shell% "su -c cp -R /data/data/com.amazon.tv.settings /%sdcard%/com.amazon.tv.settings.v2"
 %shell% "su -c cp -R /data/data/com.amazon.venezia /%sdcard%/com.amazon.venezia"
 
 %shell% "su -c mkdir -p %sdcard%/com.amazon.device.settings"
@@ -66,6 +70,8 @@ echo.
 %shell% "su -c mkdir -p %sdcard%/com.amazon.tv.launcher"
 %shell% "su -c mkdir -p %sdcard%/com.amazon.tv.oobe"
 %shell% "su -c mkdir -p %sdcard%/com.amazon.tv.settings"
+%shell% "su -c mkdir -p %sdcard%/com.amazon.tv.settings.core"
+%shell% "su -c mkdir -p %sdcard%/com.amazon.tv.settings.v2"
 %shell% "su -c mkdir -p %sdcard%/com.amazon.venezia"
 
 %pull% %sdcard%/com.amazon.device.settings %dumpFolder%/com.amazon.device.settings
@@ -74,6 +80,8 @@ echo.
 %pull% %sdcard%/com.amazon.tv.launcher %dumpFolder%/com.amazon.tv.launcher
 %pull% %sdcard%/com.amazon.tv.oobe %dumpFolder%/com.amazon.tv.oobe
 %pull% %sdcard%/com.amazon.tv.settings %dumpFolder%/com.amazon.tv.settings
+%pull% %sdcard%/com.amazon.tv.settings %dumpFolder%/com.amazon.tv.settings.core
+%pull% %sdcard%/com.amazon.tv.settings %dumpFolder%/com.amazon.tv.settings.v2
 %pull% %sdcard%/com.amazon.venezia %dumpFolder%/com.amazon.venezia
 
 start "" explorer %dumpFolder%
